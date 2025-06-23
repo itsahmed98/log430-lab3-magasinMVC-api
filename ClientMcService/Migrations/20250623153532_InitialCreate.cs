14b3e6716f5e90ef532c40986851ec5f41c321a6
itsahmed98 <ahmed.akram98@yahoo.ca>
2025-06-23 12:07:33 -0400

    Creer le microservice ClientMcService pour la gestion des clients

---
+++ ClientMcService/Migrations/20250623153532_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ClientMcService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "text", nullable: false),
+                    Courriel = table.Column<string>(type: "text", nullable: false),
+                    Adresse = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Clients",
+                columns: new[] { "ClientId", "Adresse", "Courriel", "Nom" },
+                values: new object[,]
+                {
+                    { 1, "", "alice@dupont.ca", "Alice Dupont" },
+                    { 2, "", "alex@alexandre.ca", "Alex Alexandre" },
+                    { 3, "", "chris@christopher.ca", "Chris Christopher" },
+                    { 4, "", "simon@samuel.ca", "Simon Samuel" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
